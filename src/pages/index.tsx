9855bb6baeba434913273ea4a11c3fd3846f44c3
Bruno Barbosa <bruno.barbosa.cs@gmail.com>
2021-02-21 23:02:16 -0300

    Server Side Rendering (SSR)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/Home';
 
 interface IProduct {
@@ -6,15 +6,12 @@ interface IProduct {
   title: string;
 }
 
-export default function Home() {
-  const [recommendedProducts, setRecommendedProducts] = useState<IProduct>([]);
+interface IHomeProps {
+  recommendedProducts: IProduct[];
+}
 
-  useEffect(() => {
-    fetch('http://localhost:3333/recommended')
-    .then(res => res.json())
-    .then(setRecommendedProducts);
-  }, []);
-
+export default function Home({ recommendedProducts }: IHomeProps) {
+  console.log(recommendedProducts);
   return (
     <div>
       <section>
@@ -30,4 +27,15 @@ export default function Home() {
       </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+  
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
