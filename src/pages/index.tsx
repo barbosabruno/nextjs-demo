acebe569fa1fd4ec9dfdd4b1f91f04da42f00c78
Bruno Barbosa <bruno.barbosa.cs@gmail.com>
2021-02-21 22:40:09 -0300

    Client Side Fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended')
+    .then(res => res.json())
+    .then(setRecommendedProducts);
+  }, []);
+
   return (
     <div>
-      <Title>Hello World</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(r => (
+            <li key={r.id}>
+              {r.title}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   )
 }
